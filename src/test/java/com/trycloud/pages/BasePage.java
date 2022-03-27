670936cdba1eb45cf36c63081fe256af5c92527e
husseinhadi <hadi.hussein15@gmail.com>
2022-03-27 17:41:52 -0400

    page and step definition done

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -14,17 +14,7 @@ public class BasePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//button[.='View all orders']")
-    public WebElement viewAllOrders;
 
-    @FindBy(xpath = "//button[.='View all products']")
-    public WebElement viewAllProducts;
-
-    @FindBy(xpath = "//button[.='Order']")
-    public WebElement order;
-
-    @FindBy(xpath = "//button[.='Logout']")
-    public WebElement logoutButton;
 
 
 
